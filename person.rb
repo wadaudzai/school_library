a2679af8d5605dce6ea1fd373e70bb3cee0e6a06
wadaudzai <wadaudzai@gmail.com>
2022-11-19 03:25:47 -0500

    Added Basics UI

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name, parent_permission)
     super()
     @id = Random.rand(1...1000)
     @name = name
@@ -34,13 +34,9 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
-capitalized_trimmed_person.correct_name
-
+person = Person.new(22, 'maximilianus', true)
 puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
