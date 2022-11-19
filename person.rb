387229572cd97b5485c77aadba1b2f97c65dc3f7
wadaudzai <wadaudzai@gmail.com>
2022-11-19 05:14:17 -0500

    Make changes as per reviewers request.

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name, parent_permission)
+  def initialize(age, name = "Unknown", parent_permission: true)
     super()
     @id = Random.rand(1...1000)
     @name = name
@@ -33,10 +33,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus', true)
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
