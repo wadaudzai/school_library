65404260679edff795685386ed89d38a238ba443
wadaudzai <wadaudzai@gmail.com>
2022-11-24 11:48:44 -0500

    Linter errors fixed.

--- spec/cap_decorator_spec.rb
+++ spec/cap_decorator_spec.rb
@@ -2,11 +2,11 @@ require_relative '../cap_decorator'
 require_relative '../person'
 
 describe 'Cap_decorator' do
-    context 'should create an instance of Cap_Decorator class' do
-        it 'Should return a capitalized person name' do
-            person = Person.new(23, 'walid')
-            capitalized = CapitalizeDecorator.new(person)
-            expect(capitalized.correct_name) == 'walid'.capitalize
-          end
+  context 'should create an instance of Cap_Decorator class' do
+    it 'Should return a capitalized person name' do
+      person = Person.new(23, 'walid')
+      capitalized = CapitalizeDecorator.new(person)
+      expect(capitalized.correct_name) == 'walid'.capitalize
     end
-    end
+  end
+end
