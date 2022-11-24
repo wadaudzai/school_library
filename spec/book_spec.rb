c8da6edfd43cd2170800eb35fc9f5a2d9ad517df
wadaudzai <wadaudzai@gmail.com>
2022-11-24 12:43:15 -0500

    Make changes as per reviewers request.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,5 +7,12 @@ describe 'Book' do
     it 'takes two parameters and returns a Book object' do
       expect(book.class) == Book
     end
+    it 'returns correct names of book object instance variables' do
+      expect(book.title).to eq 'Hobbit'
+      expect(book.author).to eq 'Jhon'
+    end
+    it 'should return true if the age is greater than or equal to 18' do
+      expect(book.rentals).to eq([])
+    end
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,7 +2,7 @@ require_relative '../person'
 
 describe 'Person' do
   context 'should create an instance of Person class' do
-    person = Person.new(20, 'John')
+    person = Person.new(20, 'John', true)
 
     it 'takes two parameters and returns a Person object' do
       expect(person.class) == Person
@@ -15,6 +15,22 @@ describe 'Person' do
 
     it 'should return true if the age is greater than or equal to 18' do
       expect(person.can_use_services?).to eq(true)
+    end
+
+    context 'should create an instance of Person class' do
+      peter = Person.new(19, 'Peter', false)
+      it 'Correct name' do
+        expect(peter.correct_name).to eq 'Peter'
+      end
+      it 'Use Service' do
+        expect(peter.can_use_services?).to eq true
+      end
+      it 'should have rentals' do
+        expect(person.rentals).to eq([])
+      end
+      it 'should be of age' do
+        expect(peter.of_age?).to eq true
+      end
     end
   end
 end
