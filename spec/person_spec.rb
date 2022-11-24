ef83ce692f9936fa123f43298c5f2f0b945d357b
wadaudzai <wadaudzai@gmail.com>
2022-11-24 07:31:34 -0500

    Install rspec and create unit tests for book, person, student, teacherr.

---
+++ spec/person_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../person'
+
+describe 'Person' do
+    context 'should create an instance of Person class' do
+        person = Person.new(20, 'John')
+
+        it 'takes two parameters and returns a Person object' do
+            expect(person.class) == Person
+        end
+
+        it 'returns correct names of person object instance variables' do
+            expect(person.age).to eq 20
+            expect(person.name).to eq 'John'
+          end
+
+        it 'should return true if the age is greater than or equal to 18' do
+            expect(person.can_use_services?).to eq(true)
+        end
+    end
+end
+
---
+++ spec/student_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../student'
+describe 'Student' do
+    context 'should create an instance of Student class' do
+        student = Student.new(20, 'Classroom', 'John', true)
+    
+        it 'takes four parameters and returns a Student object' do
+        expect(student.class) == Student
+        end
+
+        it 'returns correct names of student object instance variables' do
+        expect(student.age).to eq 20
+        expect(student.classroom).to eq 'Classroom'
+        expect(student.name).to eq 'John'
+        end
+
+        it 'should return true if the age is greater than or equal to 18' do
+        expect(student.can_use_services?).to eq(true)
+        end
+    end
+end
